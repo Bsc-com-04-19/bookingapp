48de430c5b8ace16ff9d9ad5c3aff31488627712
kingnjotcha <isaackbanda112@gmail.com>
2024-09-04 02:12:47 +0200

    status200

--- controllers/authController.js
+++ controllers/authController.js
@@ -49,7 +49,8 @@ exports.login = async (req, res) => {
       expiresIn: '1h',
     });
 
-    res.json({ token });
+    // res.json({ token });
+    return res.status(200).json({ token });
   } catch (error) {
     res.status(500).json({ error: 'Server error' });
   }
